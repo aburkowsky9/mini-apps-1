8829af94b6dac52d8c772a2dec465aac49482f82
aburkowsky9 <aburkowsky9@gmail.com>
2018-10-15 16:41:17 -0700

    complete min requirements, cont work on tally and winner first move

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,40 +1,65 @@
-//set up click listener for reset button!!!
-//account for clicks on positioned areas
+//account for clicks on positioned areas!!!!!!!!!!!!!!!
 
 //keep track of move count
 let moveCt = 0;
-const board = {
-  1: [null, null, null], 
-  2: [null, null, null],
-  3: [null, null, null] 
+let board = {
+  0: [null, null, null], 
+  1: [null, null, null],
+  2: [null, null, null] 
 }
-//set up click listener for each <td>
-var positions = document.getElementsByTagName('td');
+const positions = document.getElementsByTagName('td');
+const button = document.getElementById('reset-button');
 
-for (var i = 0; i < positions.length; i++) { 
-  positions[i].addEventListener('click', (event) => {
-    if (moveCt < 9) {
-      moveExecuter(event, adjustBoard, winCalculator);
+const positionClickHandler = (event) => {
+  //if moveCt is less than 9 and position is not already taken
+  if (moveCt < 9 && !event.target.innerHTML) {
+    moveExecuter(event);
+  }
+};
+
+//set up click listener for each <td>
+document.addEventListener('DOMContentLoaded', () => {
+  for (let i = 0; i < positions.length; i++) { 
+    positions[i].addEventListener('click', positionClickHandler);
+  }
+
+  //set up click listener for reset button
+  button.addEventListener('click', () => {
+    //reset memory board, moveCt, and clear positions
+    board = {
+      0: [null, null, null],
+      1: [null, null, null],
+      2: [null, null, null] 
+    };
+    
+    moveCt = 0;
+    //reset win or tie message in footer
+    document.getElementById('announcement').innerHTML = '';
+    
+    for (let i = 0; i < positions.length; i++) { 
+      positions[i].innerHTML = '';
+      positions[i].addEventListener('click', positionClickHandler);
     }
   });
-}
+});
 
-
-const moveExecuter = (event, adjustMemBoard, calculator) => {
+const moveExecuter = (event) => {
   //increment move counter and prevent moves after 9
+  var moveType;
   moveCt++;
-  
   //alternate even/odd moves for players
   if (moveCt % 2 !== 0) {
     event.target.innerHTML = 'X';
-    adjustMemBoard(event.target.id, 'X');
+    moveType = 'X';
+    adjustBoard(event.target.id, 'X');
   } else {
     event.target.innerHTML = 'O';
-    adjustMemBoard(event.target.id, 'O');
+    moveType = 'O';
+    adjustBoard(event.target.id, 'O');
   }
 
   if (moveCt >= 3) {
-    calculator();
+    checkForWin(event.target.id, moveType);
   }
 };
 
@@ -42,13 +67,71 @@ const adjustBoard = (id, input) => {
   const split = id.split(',');
   const [row, column] = split;
   
-  board[row][column - 1] = input;
-  console.log(board);
+  board[row][column] = input;
 };
 
-const winCalculator = () => {
-  //check rows
+//check for wins via rows, columns, or diagonals
+const checkForWin = (id, moveType) => {
+  const split = id.split(',');
+  const [row, column] = split;
+  var rowWin = hasRow(row, moveType);
+  var columnWin = hasColumn(column, moveType);
+  var diagWin;
   
-  //check columns
-  //check diagonals
+  if (onDiagonal(row, column)) {
+    diagWin = hasDiagonal(row, column, moveType);
+  }
+  
+  if (rowWin || columnWin || diagWin) {
+    console.log('WIN!')
+    for (var i = 0; i < positions.length; i++) { 
+      positions[i].removeEventListener('click', positionClickHandler);
+    }
+    //announce win!!!
+    announcement('win', moveType);
+  } else if (moveCt === 9) {
+    announcement('Tie')
+  }  
 };
+
+const hasRow = (row, moveType) => {
+  return hasWon(board[row], moveType);
+};
+
+const hasColumn = (column, moveType) => {
+  let columnArr = [];
+  for (var row in board) {
+    columnArr.push(board[row][column]);
+  }
+  return hasWon(columnArr, moveType);
+};
+
+const onDiagonal = (row, column) => {
+  return (row === column || (row === '0' && column === '2') || (row === '2' && column === '0'));
+};
+
+const hasDiagonal = (row, column, moveType) => {
+  var major = [board[0][0], board[1][1], board[2][2]];
+  var minor = [board[0][2], board[1][1], board[2][0]];
+  var hasDiagonalWin = hasWon(major, moveType) || hasWon(minor, moveType);
+  
+  return hasDiagonalWin;
+};
+
+//win calculator reducer for rows, columns, diagonals
+const hasWon = (array, moveType) => {
+  return array.reduce((acc, elem) => {
+    return (acc && (elem === moveType));
+  }, true);
+};
+
+const announcement = (winOrTie, moveType) => {
+  var newParagraph = document.createElement('p');
+  var announcementDiv = document.getElementById('announcement');
+  if (winOrTie === 'win') {
+    newParagraph.innerHTML = `Player ${moveType} has taken the ${winOrTie}!!!`;
+  } else {
+    newParagraph.innerHTML = `${winOrTie}: There are no winners or losers here, but we all tried our best!`;
+  }
+  announcementDiv.appendChild(newParagraph);
+}
