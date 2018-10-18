110c87b0078efab2e5e5fa4ed513d2884a07842f
aburkowsky9 <aburkowsky9@gmail.com>
2018-10-17 19:29:18 -0700

    Prototype of front-end working, start back-end

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,153 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentForm: Form1,
+      fullName: '',
+      email: '',
+      password: '', 
+      shippingAddress: '',
+      phone: '', 
+      creditNum: '',
+      expDate: '',
+      cvv: '',
+      zip: ''
+    }
+    
+    this.handleInput = this.handleInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.nextOnClick = this.nextOnClick.bind(this);
+  }
+  
+  //methods
+  handleInput(event) {
+    let stateKey = event.target.className;
+    let stateObj = {};
+    stateObj[stateKey] = event.target.value;
+    this.setState(stateObj);
+  }
+  
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state);
+  }
+  
+  nextOnClick(event) {
+    event.preventDefault();
+    if (this.state.currentForm.name === 'Form1') {
+      this.setState({ currentForm: Form2 });
+    } else if (this.state.currentForm.name === 'Form2') {
+      this.setState({ currentForm: Form3 });
+    } else if (this.state.currentForm.name === 'Form3') {
+      this.setState({ currentForm: Purchase });
+    } 
+  
+  }
+  
+  render() {
+    let TagName = this.state.currentForm || Form1;
+    return (
+      <div>
+        <TagName nextOnClick={ this.nextOnClick } handleInput={ this.handleInput } handleSubmit={this.handleSubmit } purchaseInfo={this.state} />
+      </div>
+    )
+  }
+}
 
 
+const Form1 = (props) => {
+  return (
+    <form type="submit" value="Submit">
+      <div>
+        <label>Name:
+        <input type="text" name="fullName" className="fullName" onChange={ (event) => props.handleInput(event) }/>
+        </label>
+      </div>
+      <div>
+        <label>Email:
+        <input type="email" name="email" className="email" required onChange={ (event) => props.handleInput(event) }/>
+        </label>
+      </div>
+      <div>
+        <label>Password:
+        <input type="password" name="password" className="password" onChange={ (event) => props.handleInput(event) }/>
+        </label>
+      </div>
+      <div>
+        <button onClick={ (event) => props.nextOnClick(event) }>Next</button>
+      </div>
+    </form>
+  )
+}
+
+const Form2 = (props) => {
+  return (
+    <form type="submit" value="Submit">
+      <div>
+        <label>Shipping Address:
+        <textarea rows="3" columns="10" name="shippingAddress" className="shippingAddress" onChange={ (event) => props.handleInput(event) }>
+        </textarea>
+        </label>
+      </div>
+      <div>
+        <label>Phone:
+        <input type="text" name="phone" className="phone" onChange={ (event) => props.handleInput(event) }/>
+        </label>
+      </div>
+      <div>
+        <button onClick={ (event) => props.nextOnClick(event) }>Next</button>
+      </div>
+    </form>
+  )
+}
+
+const Form3 = (props) => {
+  return (
+    <form type="submit" value="Submit">
+      <div>
+        <label>Credit Card Number:
+        <input type="text" name="creditNum" className="creditNum" onChange={ (event) => props.handleInput(event) }/>
+        </label>
+      </div>
+      <div>
+        <label>Exp Date:
+        <input type="text" name="expDate" className="expDate" required onChange={ (event) => props.handleInput(event) }/>
+        </label>
+      </div>
+      <div>
+        <label>CVV:
+        <input type="text" name="cvv" className="cvv" onChange={ (event) => props.handleInput(event) }/>
+        </label>
+      </div>
+      <div>
+        <label>Billing Zip:
+        <input type="text" name="zip" className="zip" onChange={ (event) => props.handleInput(event) }/>
+        </label>
+      </div>
+      <div>
+        <button onClick={ (event) => props.nextOnClick(event) }>Next</button>
+      </div>
+    </form>
+  )
+}
+
+const Purchase = (props) => {
+  return (
+    <div>
+      <h3>Click Purchase For Submission</h3>
+      <form type="submit" value="Submit" action="/purchases" method="POST">
+        <div>
+          <label>Shipping Address:
+          <textarea rows="10" columns="10" name="shippingAddress" className="shippingAddress" onChange={ (event) => props.handleInput(event) }>{ JSON.stringify(props.purchaseInfo) }
+          </textarea>
+          </label>
+        </div>
+        <div>
+          <button onSubmit={ (event) =>  {event.preventDefault(); console.log(App.state)} }>Purchase</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,7 @@
-
+const express = require('express');
+const app = express();
+const port = 3000;
+app.listen(port, () => console.log(`Listening on http://localhost:${port}/`))
+app.use(express.static('public'));
+app.use(express.static('node_modules'));
 
